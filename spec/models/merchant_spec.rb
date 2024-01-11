be26f3ae4f4c00edfad3675eb82befc5ce8e2a1d
Nathan Lambertson <lambertones@gmail.com>
2024-01-11 16:38:03 -0600

    add association

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoice_items).through(:items) }
     it { should have_many(:customers).through(:invoices) }
     it { should have_many(:transactions).through(:invoices) }
+    it { should have_many(:coupons) }
   end
 
   describe "associations" do
