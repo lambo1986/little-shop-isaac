87324bfea8109bf4c8b31b2365b5b4f6d77dc635
Nathan Lambertson <lambertones@gmail.com>
2024-01-11 19:38:14 -0600

    fix more test bugs from last project

--- spec/features/admin/admin_dashboard_spec.rb
+++ spec/features/admin/admin_dashboard_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "admin dashboard" do
-  before(:each) do 
+  before(:each) do
     @merch_1 = Merchant.create!(name: "Walmart", status: :enabled)
     @merch_2 = Merchant.create!(name: "Target", status: :enabled)
 
@@ -24,7 +24,7 @@ RSpec.describe "admin dashboard" do
     @inv_4 = @cust_4.invoices.create!(status: :completed)
     @inv_5 = @cust_5.invoices.create!(status: :in_progress,created_at:Time.new(2024, 02, 1))
     @inv_6 = @cust_6.invoices.create!(status: :cancelled, created_at:Time.new(2021, 10, 31))
-   
+
     @tran_1 = @inv_1.transactions.create!(credit_card_number: "2222 2222 2222 2222", credit_card_expiration_date: "01/2021", result: :success )
     @tran_2 = @inv_2.transactions.create!(credit_card_number: "2222 2222 2222 2222", credit_card_expiration_date: "02/2022", result: :success )
     @tran_3 = @inv_3.transactions.create!(credit_card_number: "2222 2222 2222 2222", credit_card_expiration_date: "03/2023", result: :success )
@@ -32,10 +32,10 @@ RSpec.describe "admin dashboard" do
     @tran_5 = @inv_5.transactions.create!(credit_card_number: "2222 2222 2222 2222", credit_card_expiration_date: "05/2025", result: :success )
     @tran_6 = @inv_6.transactions.create!(credit_card_number: "2222 2222 2222 2222", credit_card_expiration_date: "06/2026", result: :failed )
 
-   @ii_1 = InvoiceItem.create!(invoice: @inv_1, item: @item_1, quantity: 10, unit_price: 1, status: :shipped )
-   @ii_2 = InvoiceItem.create!(invoice: @inv_1, item: @item_2, quantity: 10, unit_price: 1, status: :shipped)
-   @ii_3 = InvoiceItem.create!(invoice: @inv_1, item: @item_3, quantity: 10, unit_price: 1, status: :shipped )
-   @ii_4 = InvoiceItem.create!(invoice: @inv_2, item: @item_1, quantity: 10, unit_price: 1, status: :shipped )
+    @ii_1 = InvoiceItem.create!(invoice: @inv_1, item: @item_1, quantity: 10, unit_price: 1, status: :shipped )
+    @ii_2 = InvoiceItem.create!(invoice: @inv_1, item: @item_2, quantity: 10, unit_price: 1, status: :shipped)
+    @ii_3 = InvoiceItem.create!(invoice: @inv_1, item: @item_3, quantity: 10, unit_price: 1, status: :shipped )
+    @ii_4 = InvoiceItem.create!(invoice: @inv_2, item: @item_1, quantity: 10, unit_price: 1, status: :shipped )
     @ii_5 = InvoiceItem.create!(invoice: @inv_2, item: @item_2, quantity: 10, unit_price: 1, status: :shipped)
     @ii_6 = InvoiceItem.create!(invoice: @inv_2, item: @item_3, quantity: 10, unit_price: 1, status: :shipped )
     @ii_7 = InvoiceItem.create!(invoice: @inv_3, item: @item_1, quantity: 10, unit_price: 1, status: :shipped )
@@ -61,7 +61,7 @@ RSpec.describe "admin dashboard" do
       expect(page).to have_content("Admin Dashboard")
     end
 
-    it "has links to admin merchants and routes a user" do 
+    it "has links to admin merchants and routes a user" do
 
       visit admin_root_path
 
@@ -70,9 +70,9 @@ RSpec.describe "admin dashboard" do
       click_link ("Admin Merchants")
 
       expect(current_path).to eq(admin_merchants_path)
-    end 
+    end
 
-    it "has links to admin invoices and routes a user" do 
+    it "has links to admin invoices and routes a user" do
 
       visit admin_root_path
 
@@ -81,17 +81,13 @@ RSpec.describe "admin dashboard" do
       click_link ("Admin Invoices")
 
       expect(current_path).to eq(admin_invoices_path)
-    end 
+    end
 
     # 21. Admin Dashboard Statistics - Top Customers
-    it "shows the top 5 customers and the amount of their transactions" do 
-      # require 'pry'; binding.pry
-      # As an admin,
-      # When I visit the admin dashboard (/admin)
+    it "shows the top 5 customers and the amount of their transactions" do
+
       visit admin_root_path
-      # Then I see the names of the top 5 customers
-      # who have conducted the largest number of successful transactions
-      # And next to each customer name I see the number of successful transactions they have conducted
+
       within '.top-five' do
         expect(page).to have_content("#{@cust_1.first_name} #{@cust_1.last_name} - 1 purchases")
         expect(page).to have_content("#{@cust_2.first_name} #{@cust_2.last_name} - 1 purchases")
@@ -106,27 +102,19 @@ RSpec.describe "admin dashboard" do
 
       visit admin_root_path
 
-      
-
       expect(page).to have_content("Invoice#-#{@ii_15.invoice_id}")
       expect(page).to_not have_content(@ii_2.invoice_id)
+      expect(page).to have_link(@ii_15.invoice_id.to_s)
 
-      expect(page).to have_link(@ii_15.invoice_id)
-
-      click_link @ii_15.invoice_id
+      click_link @ii_15.invoice_id.to_s
 
       expect(current_path).to eq(admin_invoice_path(@ii_15.invoice))
-
-    
-
-
     end
 
-    it "list the date in order" do 
+    it "list the date in order" do
       visit admin_root_path
 
       expect("Invoice#-#{@ii_19.invoice_id}").to appear_before("Invoice#-#{@ii_15.invoice_id}")
-
     end
   end
 end
