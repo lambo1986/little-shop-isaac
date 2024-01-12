c79741ea8e40dbb8201441a03c1227e4cfdb89a9
Nathan Lambertson <lambertones@gmail.com>
2024-01-12 14:23:54 -0600

    Add tests for activating coupon with a button

--- spec/features/merchants/coupons/show_spec.rb
+++ spec/features/merchants/coupons/show_spec.rb
@@ -38,6 +38,28 @@ RSpec.describe "Merchant's coupons show page", type: :feature do
       expect(page).to have_content("Active? false")
       expect(page).to_not have_button("deactivate")
     end
+
+    it "has a button to activate the coupon" do #US-5
+      merchant1 = Merchant.create!(name: "Sweetwater", status: :enabled)
+      bob = Customer.create!(first_name: "Bob", last_name: "Smith")
+      coupon1 = merchant1.coupons.create!(name: "Buy One Get One 50%", code: "BOGO50", percent_off: 50, dollar_off: 0, active: false)
+      invoice1 = coupon1.invoices.create!(status: 2, customer: bob)
+      transaction1 = invoice1.transactions.create!(credit_card_number: "1234567854", credit_card_expiration_date: "12/20", result: 0)
+
+      visit "/merchants/#{merchant1.id}/coupons/#{coupon1.id}"
+
+      expect(page).to have_content("Coupon: Buy One Get One 50%")
+      expect(page).to have_content("Active? false")
+      expect(page).to have_button("activate")
+
+      click_button("activate")
+
+      expect(current_path).to eq("/merchants/#{merchant1.id}/coupons/#{coupon1.id}")
+      expect(page).to have_content("update successful")
+      expect(page).to have_content("Coupon: Buy One Get One 50%")
+      expect(page).to have_content("Active? true")
+      expect(page).to have_button("deactivate")
+    end
   end
 
   describe "when the coupon is active" do
