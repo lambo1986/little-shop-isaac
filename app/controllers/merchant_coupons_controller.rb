1d30ee65dbd8e4ae71492c5ee641a2809eb0e6ae
Nathan Lambertson <lambertones@gmail.com>
2024-01-11 21:50:00 -0600

    add new and create method for merchant coupons controller

--- app/controllers/merchant_coupons_controller.rb
+++ app/controllers/merchant_coupons_controller.rb
@@ -7,4 +7,26 @@ class MerchantCouponsController < ApplicationController
   def show
     @merchant_coupon = Coupon.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @coupon = Coupon.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @coupon = @merchant.coupons.new(coupon_params)
+    if @coupon.save
+      redirect_to "/merchants/#{@merchant.id}/coupons", notice: "Coupon was successfully created."
+    else
+      redirect_to "/merchants/#{@merchant.id}/coupons/new"
+      flash[:alert] = "Error: all fields are required or must be unique." #needs work
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :code, :percent_off, :dollar_off)
+  end
 end
