680a62f98bf222dbada1d53ab16ffbd69fb9adb9
Nathan Lambertson <lambertones@gmail.com>
2024-01-12 12:25:20 -0600

    Add merchant  to show function

--- app/controllers/merchant_coupons_controller.rb
+++ app/controllers/merchant_coupons_controller.rb
@@ -6,6 +6,7 @@ class MerchantCouponsController < ApplicationController
 
   def show
     @merchant_coupon = Coupon.find(params[:id])
+    @merchant = Merchant.find(@merchant_coupon.merchant_id)
   end
 
   def new
@@ -27,6 +28,18 @@ class MerchantCouponsController < ApplicationController
         flash[:alert] = "Only 5 active coupons can be used at a time."
       end
       redirect_to "/merchants/#{@merchant.id}/coupons/new"
+    end
+  end
+
+  def update
+    @merchant_coupon = Coupon.find(params[:id])
+    if params[:commit] == "deactivate"
+      if @merchant_coupon.invoice_in_progress?
+        redirect_to "/merchants/#{@merchant_coupon.merchant_id}/coupons/#{@merchant_coupon.id}", alert: "update failed do to pending invoice"
+      else
+        @merchant_coupon.update!(active: false)
+        redirect_to "/merchants/#{@merchant_coupon.merchant_id}/coupons/#{@merchant_coupon.id}", notice: "update successful"
+      end
     end
   end
 
