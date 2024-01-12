9b0e80b00f7cc922f2abb25c13a2eb194c54e0e9
Nathan Lambertson <lambertones@gmail.com>
2024-01-12 10:23:56 -0600

    add test for new association and model method for US-3

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -21,5 +21,24 @@ RSpec.describe Coupon, type: :model do
   describe "associations" do
     it { should belong_to(:merchant) }
     it { should have_many(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
+  end
+
+  describe "instance methods" do
+    it "has a #times_used method" do
+      merchant1 = Merchant.create!(name: "Sweetwater", status: :enabled)
+      bob = Customer.create!(first_name: "Bob", last_name: "Smith")
+      jerry = Customer.create!(first_name: "Jerry", last_name: "Jones")
+      tina = Customer.create!(first_name: "Tina", last_name: "Turner")
+      coupon1 = merchant1.coupons.create!(name: "Buy One Get One 50%", code: "BOGO50", percent_off: 50, dollar_off: 0, active: true)
+      invoice1 = coupon1.invoices.create!(status: 2, customer: bob)
+      invoice2 = coupon1.invoices.create!(status: 2, customer: jerry)
+      invoice3 = coupon1.invoices.create!(status: 2, customer: tina)
+      transaction1 = invoice1.transactions.create!(credit_card_number: "1234567854", credit_card_expiration_date: "12/20", result: 0)
+      transaction2 = invoice2.transactions.create!(credit_card_number: "1236785476", credit_card_expiration_date: "12/23", result: 0)
+      transaction2 = invoice3.transactions.create!(credit_card_number: "1236785498", credit_card_expiration_date: "12/22", result: 1)#failed
+
+      expect(coupon1.times_used).to eq(2)
+    end
   end
 end
